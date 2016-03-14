9f34ea970a7919d378be8a78f519477cc85ef662
ankh2054 <charles.holtzkampf@gmail.com>
2016-03-14 13:01:50 +0000

    Update bomel.c

--- bomel.c
+++ bomel.c
@@ -1,3 +1,9 @@
+/*
+ * Compile and use in conjunction with UDF to create shell as root. 
+ * Must set owner to root (chmod root:root bomel) and set appropriate permissions chmod 4777 ( Sets SUID and allows everyone to run)
+ *
+ */
+
 #include <stdio.h>
 #include <sys/types.h>
 #include <unistd.h>
